c76f8e8b61ccd86051f8420bd5e7b80c061192c8
Madeline Garcia-Cabrera <vmgarcia.cabrera21@gmail.com>
2022-04-05 20:40:15 -0400

    typesDefs

---
+++ server/schemas/typesDefs.js
@@ -0,0 +1,54 @@
+const { gql } = require('apollo-server-express');
+
+
+const typeDefs = gql`
+
+type User {
+    _id: ID
+    firstName: String
+    lastName: String
+    email: String
+    status:[User]
+  }
+
+  type Comment {
+      _id:ID
+      commenttext:String
+      User:String
+  }
+
+  type Auth {
+    token: ID
+    user: User
+  }
+
+  type Habit {
+      _id: ID
+      name:String
+      description:String
+  }
+
+  type Status{
+    _id: ID
+    statusText:String
+    username:String
+    createdAt:String
+    comments:[Comment]
+  }
+
+  type Query{
+    me:User
+    users:[User]
+    user(username:String!): User
+    comments(username:String): [Comment]
+    comment(_id:ID!): Comment
+  }
+
+  type Mutation {
+    login(email: String!, password: String!): Auth
+    addUser(username: String!, email: String!, password: String!): Auth
+    addComment(commentText: String!): Status
+    addStatus(statusText: String!): Status
+    
+  }
+  `;
