18910a1c263ddd93bff46dd121892b6b92fc330e
Alexys Carrasquillo <alexys.carrasquillo@gmail.com>
2022-04-06 17:33:17 -0400

    set up utils folder and mutations

---
+++ client/src/utils/mutations.js
@@ -0,0 +1,57 @@
+import gql from "graphql-tag";
+
+export const LOGIN_USER = gql`
+mutation login($email: String!, $password: String!) {
+    login(email: $email, password: $password) {
+        token
+        user {
+            _id
+            firstName
+        }
+    }
+}
+`;
+
+export const ADD_USER = gql`
+mutation addUser($username: String!, $email: String!, password: String!) {
+    addUser(username: $username, email: $email, password: $password) {
+        token
+        user {
+            _id
+            firstName
+        }
+    }
+}
+`;
+
+export const ADD_COMMENT = gql`
+mutation addComment($commentText: String!) {
+    addComment(commentText: $commentText) {
+        _id
+        statusText
+        username
+        createdAt
+        comments {
+            _id
+            commentText
+            User
+        }
+    }
+}
+`;
+
+export const ADD_STATUS = gql`
+mutation addStatus($statusText: String!) {
+    addStatus(statusText = $statusText) {
+        _id
+        statusText
+        username
+        createdAt
+        comments {
+            _id
+            commentText
+            User
+        }
+    }
+}
+`;
