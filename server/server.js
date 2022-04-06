652789c283997e31c1a3ff4582c7d71e64d7f52a
jclong <j.mail1014@gmail.com>
2022-04-05 20:56:16 -0400

    refactored server.js file

--- server/server.js
+++ server/server.js
@@ -1,22 +1,40 @@
-const express = require("express");
-const mongoose = require("mongoose");
+const express = require('express');
+const {ApolloServer} = require('apollo-server-express');
+const path = require('path');
+const db = require('./config/connection');
+const routes = require('./routes');
+
+
+const {typeDefs, resolvers} = require('./schemas');
+const {authMiddleware} = require('./utils/auth');
 
 const app = express();
 const PORT = process.env.PORT || 3001;
 
-app.use(express.json());
+const startServer = async () => {
+  const server = new ApolloServer({
+    typeDefs,
+    resolvers,
+    context: authMiddleware,
+  });
+  await server.start();
+  server.applyMiddleware({ app });
+  console.log(`Use GraphQL at http://localhost:${PORT}${server.graphqlPath}`);
+};
+
+startServer()
+
+
 app.use(express.urlencoded({ extended: true }));
-app.use(express.static("public"));
+app.use(express.json());
 
-mongoose.connect(process.env.MONGODB_URI || "mongodb://localhost/pizza-hunt", {
-  useFindAndModify: false,
-  useNewUrlParser: true,
-  useUnifiedTopology: true,
+// if we're in production, serve client/build as static assets
+if (process.env.NODE_ENV === 'production') {
+  app.use(express.static(path.join(__dirname, '../client/build')));
+}
+
+app.use(routes);
+
+db.once('open', () => {
+  app.listen(PORT, () => console.log(`🌍 Now listening on localhost:${PORT}`));
 });
-
-// Use this to log mongo queries being executed!
-mongoose.set("debug", true);
-
-app.use(require("./routes"));
-
-app.listen(PORT, () => console.log(`🌍 Connected on localhost:${PORT}`));
