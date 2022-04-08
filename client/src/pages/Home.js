3710ee06343020586dba79862d54a1fbf0e245ed
jclong <j.mail1014@gmail.com>
2022-04-07 21:07:29 -0400

    create home.js

---
+++ client/src/pages/Home.js
@@ -0,0 +1,39 @@
+import React from "react";
+import { Link } from 'react-router-dom';
+
+import Auth from '../../utils/auth';
+
+const Header = () => {
+  const logout = (event) => {
+    event.preventDefault();
+    Auth.logout();
+  };
+
+  return (
+    <header>
+      <div>
+        <Link to="/">
+          <h1>Habit Social</h1>
+        </Link>
+
+        <nav>
+          {Auth.loggedIn() ? (
+            <>
+              <Link to="/profile">Me</Link>
+              <a href="/" onClick={logout}>
+                Logout
+              </a>
+            </>
+          ) : (
+            <>
+              <Link to="/login">Login</Link>
+              <Link to="/register">Signup</Link>
+            </>
+          )}
+        </nav>
+      </div>
+    </header>
+  );
+};
+
+export default Header;
