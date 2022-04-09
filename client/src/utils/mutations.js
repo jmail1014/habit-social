24d94abd61bc47c7793cedd18b772b3f72a99435
jclong <j.mail1014@gmail.com>
2022-04-09 14:04:09 -0400

    made small changes to queries

--- client/src/utils/mutations.js
+++ client/src/utils/mutations.js
@@ -1,57 +1,57 @@
 import gql from "graphql-tag";
 
 export const LOGIN_USER = gql`
-mutation login($email: String!, $password: String!) {
+  mutation login($email: String!, $password: String!) {
     login(email: $email, password: $password) {
-        token
-        user {
-            _id
-            firstName
-        }
+      token
+      user {
+        _id
+        firstName
+      }
     }
-}
+  }
 `;
 
 export const ADD_USER = gql`
-mutation addUser($username: String!, $email: String!, $password: String!) {
+  mutation addUser($username: String!, $email: String!, $password: String!) {
     addUser(username: $username, email: $email, password: $password) {
-        token
-        user {
-            _id
-            username
-        }
+      token
+      user {
+        _id
+        username
+      }
     }
-}
+  }
 `;
 
 export const ADD_COMMENT = gql`
-mutation addComment($commentText: String!) {
-    addComment(commentText: $commentText) {
+  mutation addComment($commentText: String!, $username: String!) {
+    addComment(commentText: $commentText, username: $username) {
+      _id
+      statusText
+      username
+      createdAt
+      comments {
         _id
-        statusText
-        username
-        createdAt
-        comments {
-            _id
-            commentText
-            User
-        }
+        commentText
+        User
+      }
     }
-}
+  }
 `;
 
 export const ADD_REACTION = gql`
-mutation addReaction($statusText: String!) {
-    addReaction(statusText : $statusText) {
+  mutation addReaction($statusText: String!) {
+    addReaction(statusText: $statusText) {
+      _id
+      statusText
+      username
+      createdAt
+      comments {
         _id
-        statusText
-        username
-        createdAt
-        comments {
-            _id
-            commentText
-            User
-        }
+        commentText
+        User
+      }
     }
-}
+  }
 `;
