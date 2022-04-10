c9de355cb73a867a71db4d9135a0c8f71547e0e7
jclong <j.mail1014@gmail.com>
2022-04-10 07:43:20 -0400

    fixed double header and reactDom

--- client/src/pages/Home.js
+++ client/src/pages/Home.js
@@ -3,38 +3,42 @@ import { Link } from 'react-router-dom';
 
 import Auth from '../utils/auth';
 
-const Header = () => {
+const Home = () => {
   const logout = (event) => {
     event.preventDefault();
     Auth.logout();
   };
 
   return (
-    <h3>This is the home page</h3>
-    // <header>
-    //   <div>
-    //     <Link to="/">
-    //       <h1>Habit Social</h1>
-    //     </Link>
+    <header>
+      <div>
+        <Link to="/">
+          <h1>Habit Social</h1>
+        </Link>
 
-    //     <nav>
-    //       {Auth.loggedIn() ? (
-    //         <>
-    //           <Link to="/profile">Me</Link>
-    //           <a href="/" onClick={logout}>
-    //             Logout
-    //           </a>
-    //         </>
-    //       ) : (
-    //         <>
-    //           <Link to="/login">Login</Link>
-    //           <Link to="/register">Signup</Link>
-    //         </>
-    //       )}
-    //     </nav>
-    //   </div>
-    // </header>
+        <nav>
+          {Auth.loggedIn() ? (
+            <>
+              <Link to="/profile">Me</Link>
+              <a href="/" onClick={logout}>
+                Logout
+              </a>
+            </>
+          ) : (
+            <>
+              <Link to="/login">Login</Link>
+              <Link to="/register">Signup</Link>
+            </>
+          )}
+        </nav>
+      </div>
+    </header>
+    // <>
+    // <div>
+    // <h3>This is the home page</h3>
+    // </div>
+    // </>
   );
 };
 
-export default Header;
+export default Home;
