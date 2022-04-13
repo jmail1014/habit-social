8a249c11cbabd95591ca696519cb84ce72c17d78
jclong <j.mail1014@gmail.com>
2022-04-13 12:20:43 -0400

    had to add the userauth back to profile page to fix home page

--- client/src/pages/Profile.js
+++ client/src/pages/Profile.js
@@ -1,7 +1,7 @@
 import React from "react";
-import { useParams } from "react-router-dom";
+import { Navigate, useParams } from "react-router-dom";
 
-// Navigate,
+// 
 
 import Reaction from "../components/Reaction";
 // import Habit from "../components/Habits";
@@ -9,7 +9,7 @@ import Reaction from "../components/Reaction";
 
 import { useQuery } from "@apollo/client";
 import { GET_USER, GET_ME } from "../utils/queries";
-// import Auth from "../utils/auth";
+import Auth from "../utils/auth";
 
 const Profile = (loading, props) => {
   const { username: userParam } = useParams();
@@ -20,22 +20,22 @@ const Profile = (loading, props) => {
 
   const user = data?.me || data?.user || {};
 
-  // if (Auth.loggedIn() && Auth.getProfile().data.username === userParam) {
-  //   return <Navigate to="/profile" />;
-  // }
+  if (Auth.loggedIn() && Auth.getProfile().data.username === userParam) {
+    return <Navigate to="/profile" />;
+  }
 
-  // if (loading) {
-  //   return <div>Loading...</div>;
-  // }
+  if (loading) {
+    return <div>Loading...</div>;
+  }
 
-  // if (!user?.username) {
-  //   return (
-  //     <h4>
-  //       You need to be logged in to see this. Use the navigation links above to
-  //       sign up or log in!
-  //     </h4>
-  //   );
-  // }
+  if (!user?.username) {
+    return (
+      <h4>
+        You need to be logged in to see this. Use the navigation links above to
+        sign up or log in!
+      </h4>
+    );
+  }
 
   return (
     <div>
