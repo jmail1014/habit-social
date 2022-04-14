892d122bd4bc56ce958fecac40db6d58835e5c2c
jclong <j.mail1014@gmail.com>
2022-04-14 17:13:09 -0400

    reword a statement on the homepage and update screenshot with donate

--- client/src/pages/Profile.js
+++ client/src/pages/Profile.js
@@ -33,7 +33,7 @@ const Profile = (props) => {
   if (!user?.username) {
     return (
       <h4>
-        You need to be logged in to see your profile. Use the navigation links
+        You need to be logged in to see your profile. Use the forms
         above to sign up or log in!
       </h4>
     );
